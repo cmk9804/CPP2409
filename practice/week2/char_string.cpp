3291f1758b36d431e5035e5b188d6ac82c36691a
cmk9804 <cmk9804@gmail.com>
2024-09-09 16:37:04 +0900

    Variable and Data Type Exercises

---
+++ practice/week2/char_string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    //char c = "1";
+    char s = '1';
+    string s1 = "Good";
+    string s2 = "Morning";
+    string s3 = s1 + " " + s2 + "!";
+    cout << s3 << endl;
+    return 0;
+}
