b9e73f364c8d757789bc50c7e9a865082c7b2dc1
cmk9804 <cmk9804@gmail.com>
2024-10-30 15:53:56 +0900

    Class Exercises

---
+++ practice/week9/car.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Car{
+    public:
+    int speed, gear;
+    string color;
+
+    void SpeedUp(){
+        speed += 10;
+    }
+    void SpeedDown(){
+        speed -= 10;
+    }
+};
+
+int main() {
+    Car myCar;
+
+    myCar.speed = 100;
+    myCar.gear = 3;
+    myCar.color = "red";
+    
+    cout << "1st speed: " << myCar.speed << endl;
+
+    myCar.SpeedUp();
+    cout << "2nd speed: " << myCar.speed << endl;
+
+    myCar.SpeedDown();
+    cout << "3rd speed: " << myCar.speed << endl;
+
+    return 0;
+}
